01f85cbb2436507dee0ef8538fd9f9ca852f86f3
dexterneo <jeremyfourna@gmail.com>
2015-08-14 13:03:44 +0200

    Finish tome detail page

--- client/templates/front/tomeDetails.js
+++ client/templates/front/tomeDetails.js
@@ -1,9 +1,5 @@
 Template.tomeDetails.helpers({
-	mangaData: function() {
-		return Mangas.find({
-			"user": Meteor.userId(),
-			"mangasName": this.params.mangasName,
-			"tomeNumber": Number(this.params.tomeNumber)
-		},{limit:1}).fetch()[0];
+	displayDate: function() {
+		return moment(this.releaseDate, "YYYY/MM/DD").fromNow();
 	}
 });
