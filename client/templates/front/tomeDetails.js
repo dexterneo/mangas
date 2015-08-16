acaca11b7ec0ea1510557d5c6570c03ee89d7525
dexterneo <jeremyfourna@gmail.com>
2015-08-16 13:18:13 +0200

    Add infos about the mangas and event to add or remove from collection a mangas

--- client/templates/front/tomeDetails.js
+++ client/templates/front/tomeDetails.js
@@ -6,3 +6,20 @@ Template.tomeDetails.helpers({
 		return !this.owned;
 	}
 });
+
+Template.tomeDetails.events({
+	'click #setOwnedTrue': function() {
+		Meteor.call('setOwnedTrue', this._id, function(error) {
+			if (error) {
+				return throwError(error.message);
+			}
+		});
+	},
+	'click #setOwnedFalse': function() {
+		Meteor.call('setOwnedFalse', this._id, function(error) {
+			if (error) {
+				return throwError(error.message);
+			}
+		});
+	}
+});
