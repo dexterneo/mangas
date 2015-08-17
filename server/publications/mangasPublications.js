33289e20eec39a04a81837c748d6885258696bea
dexterneo <jeremyfourna@gmail.com>
2015-08-17 14:00:10 +0200

    Create a new subscription for all the tomes of a mangas

--- server/publications/mangasPublications.js
+++ server/publications/mangasPublications.js
@@ -1,22 +1,29 @@
 // Send back all owned mangas
 Meteor.publish('allOwnedMangas', function(userId) {
-	return Mangas.find({
-		"user": userId,
-		"owned": true
-	});
+    return Mangas.find({
+        "user": userId,
+        "owned": true
+    });
 });
 
 // Send back all missing mangas
 Meteor.publish('allMissingMangas', function(userId) {
-	return Mangas.find({
-		"user": userId,
-		"owned": false
-	});
+    return Mangas.find({
+        "user": userId,
+        "owned": false
+    });
 });
 
 // Send back the data about a tome
 Meteor.publish('tomeDetails', function(id) {
-	return Mangas.find({
-		"_id": id
-	});
+    return Mangas.find({
+        "_id": id
+    });
+});
+
+Meteor.publish('allTomes', function(userId, mangasName) {
+    return Mangas.find({
+        "user": userId,
+        "mangasName": mangasName
+    });
 });
