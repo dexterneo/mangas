115454c0486a18bc0323db7ce158ead69111a819
dexterneo <jeremyfourna@gmail.com>
2015-08-17 14:00:45 +0200

    Add the all tomes part

--- client/templates/front/tomeDetails.js
+++ client/templates/front/tomeDetails.js
@@ -1,25 +1,35 @@
 Template.tomeDetails.helpers({
-	displayDate: function() {
-		return moment(this.releaseDate, "YYYY/MM/DD").fromNow();
-	},
-	notOwned: function() {
-		return !this.owned;
-	}
+    displayDate: function() {
+        return moment(this.releaseDate, "YYYY/MM/DD").fromNow();
+    },
+    notOwned: function() {
+        return !this.owned;
+    },
+    allTomes: function() {
+        return Mangas.find({
+            "user": Meteor.userId(),
+            "mangasName": this.mangasName
+        }, {
+            sort: {
+                "tomeNumber": 1
+            }
+        });
+    }
 });
 
 Template.tomeDetails.events({
-	'click #setOwnedTrue': function() {
-		Meteor.call('setOwnedTrue', this._id, function(error) {
-			if (error) {
-				return throwError(error.message);
-			}
-		});
-	},
-	'click #setOwnedFalse': function() {
-		Meteor.call('setOwnedFalse', this._id, function(error) {
-			if (error) {
-				return throwError(error.message);
-			}
-		});
-	}
+    'click #setOwnedTrue': function() {
+        Meteor.call('setOwnedTrue', this._id, function(error) {
+            if (error) {
+                return throwError(error.message);
+            }
+        });
+    },
+    'click #setOwnedFalse': function() {
+        Meteor.call('setOwnedFalse', this._id, function(error) {
+            if (error) {
+                return throwError(error.message);
+            }
+        });
+    }
 });
