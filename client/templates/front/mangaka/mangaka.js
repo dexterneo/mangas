acca89c16896da5a6ec8482a996708e0ccf49269
dexterneo <jeremyfourna@gmail.com>
2015-08-18 13:49:35 +0200

    Refactoring folder system and add mangaka page

---
+++ client/templates/front/mangaka/mangaka.js
@@ -0,0 +1,12 @@
+Template.mangaka.helpers({
+    author: function() {
+        return Router.current().params.author;
+    },
+    serie: function() {
+        return Mangas.find({
+            "user": Meteor.userId(),
+            "author": Router.current().params.author,
+            "tomeNumber": 1
+        });
+    }
+});
