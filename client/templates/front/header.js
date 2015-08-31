d37645db859b2b75247a0b6b1ed7920d1f2ab2b4
dexterneo <jeremyfourna@gmail.com>
2015-08-31 13:20:36 +0200

    Add an if currentUser to hide menu

--- client/templates/front/header.js
+++ client/templates/front/header.js
@@ -1,16 +1,16 @@
 Template.header.helpers({
-	homeActive: function() {
-		if (Router.current().route._path === '/') {
-			return 'active';
-		} else {
-			return false;
-		}
-	},
-	missingActive: function() {
-		if (Router.current().route._path === '/missingMangas') {
-			return 'active';
-		} else {
-			return false;
-		}
-	}
+    homeActive: function() {
+        if (Router.current().route._path === '/ownedMangas') {
+            return 'active';
+        } else {
+            return false;
+        }
+    },
+    missingActive: function() {
+        if (Router.current().route._path === '/missingMangas') {
+            return 'active';
+        } else {
+            return false;
+        }
+    }
 });
