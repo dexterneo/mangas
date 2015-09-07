1319197a2365e64de871d631013c788139ffccf6
dexterneo <jeremyfourna@gmail.com>
2015-09-07 13:44:01 +0200

    Add publication for addMangaForUser

--- server/publications/mangasDataPublications.js
+++ server/publications/mangasDataPublications.js
@@ -1,8 +1,19 @@
 // Send back all mangas
 Meteor.publish('allMangasCover', function() {
-    return MangasData.find({}, {
-        fields: {
-            "cover": 1
-        }
-    });
+	return MangasData.find({}, {
+		fields: {
+			"cover": 1,
+			"names.fr": 1
+		}
+	});
+});
+
+Meteor.publish('allMangasData', function() {
+	return MangasData.find({});
+});
+
+Meteor.publish('oneMangasData', function(mangaId) {
+	return MangasData.find({
+		_id: mangaId
+	});
 });
