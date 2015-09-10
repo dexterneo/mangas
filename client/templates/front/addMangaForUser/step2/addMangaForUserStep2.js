914dceb119b60d1bea6ef360704ed55119b8e784
dexterneo <jeremyfourna@gmail.com>
2015-09-10 13:52:50 +0200

    WIP add step 2 for adding a manga to a user's collection

--- client/templates/front/addMangaForUser/step2/addMangaForUserStep2.js
+++ client/templates/front/addMangaForUser/step2/addMangaForUserStep2.js
@@ -1,3 +1,9 @@
-Template.addMangaForUserStep2.helpers({});
+Template.addMangaForUserStep2.helpers({
+	manga: function() {
+		return MangasData.find({
+			'_id': Router.current().params._id
+		});
+	}
+});
 
 Template.addMangaForUserStep2.events({});
