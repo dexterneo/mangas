b7e6fdb8813181c9308f5c6ec3c4e33ae5fd9621
dexterneo <jeremyfourna@gmail.com>
2016-01-20 23:37:31 +0100

    WIP for adding tomes in mangatek

--- client/templates/front/addMangaForUser/step2/addMangaForUserStep2.js
+++ client/templates/front/addMangaForUser/step2/addMangaForUserStep2.js
@@ -6,4 +6,41 @@ Template.addMangaForUserStep2.helpers({
 	}
 });
 
-Template.addMangaForUserStep2.events({});
+Template.addMangaForUserStep2.events({
+	"click .grid-item": function(e, t) {
+		e.preventDefault();
+		return $(e.target).parents(".grid-item").find("button").toggleClass("btn-default btn-success");
+	},
+	"click .ownThemAll": function(e, t) {
+		e.preventDefault();
+		$(".ownThemAll").toggleClass("btn-primary btn-warning");
+		return $(".addToCollection").toggleClass("btn-default btn-success");
+	},
+	"click .addTomes": function(e, t) {
+		e.preventDefault();
+		$('.mangas').each(function(index, element) {
+			console.log(this);
+			/*var mangas = {
+				title: $('#title').val(),
+				user: Meteor.userId(),
+				mangasName: $('#mangasName').val(),
+				author: $('#author').val(),
+				tomeNumber: filterInt($('#tomeNumber').val()),
+				isbn: $('#isbn').val(),
+				cover: $('#cover').val(),
+				releaseDate: $('#releaseDate').val(),
+				owned: $('#owned').is(':checked'),
+				genre: $('#genre').val(),
+				editor: $('#editor').val(),
+				version: $('#version').val()
+			};
+			Meteor.call('mangasInsert', mangas, function(error) {
+				if (error) {
+					return throwError(error.message);
+				} else {
+					$('#myModal').modal('hide');
+				}
+			});*/
+		});
+	}
+});
--- client/templates/front/components/cover/cover2.js
+++
@@ -1,3 +0,0 @@
-Template.cover2.onRendered(function() {
-	$('.mangas-cover').style.padding = '0px 0px';
-})
