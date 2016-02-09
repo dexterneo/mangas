ef79a6232ae3f645c24e026feac5dfee693e8537
dexterneo <jeremyfourna@gmail.com>
2016-02-09 13:56:45 +0100

    WIP on this component

--- client/templates/front/components/addMangaToWaitingList/addMangaToWaitingList.js
+++ client/templates/front/components/addMangaToWaitingList/addMangaToWaitingList.js
@@ -2,6 +2,29 @@ class AddMangaToWaitingList extends BlazeComponent {
 	template() {
 		return 'addMangaToWaitingList';
 	}
+
+	events() {
+		return super.events().concat({
+			'click #addMangaToWaitingList': this.addMangaToWaitingList
+		});
+	}
+
+	addMangaToWaitingList() {
+		var manga = {
+			askBy: Meteor.userId(),
+			name: $('#mangasNameToAdd').val(),
+			date: new Date()
+		};
+
+		Meteor.call('addToWaitingList', manga, function(error, result) {
+			if (error) {
+				return throwError(error.message);
+			} else {
+				console.log(result);
+				$('#mangasNameToAdd').val('');
+			}
+		});
+	}
 }
 
 AddMangaToWaitingList.register('AddMangaToWaitingList');
