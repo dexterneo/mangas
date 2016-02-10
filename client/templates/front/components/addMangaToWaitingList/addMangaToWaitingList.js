1b88408f563dbdf75eeda65f466128a102d243f8
dexterneo <jeremyfourna@gmail.com>
2016-02-10 13:18:03 +0100

    Change property name of the collection

--- client/templates/front/components/addMangaToWaitingList/addMangaToWaitingList.js
+++ client/templates/front/components/addMangaToWaitingList/addMangaToWaitingList.js
@@ -11,7 +11,7 @@ class AddMangaToWaitingList extends BlazeComponent {
 
 	addMangaToWaitingList() {
 		var manga = {
-			askBy: Meteor.userId(),
+			askedBy: Meteor.userId(),
 			name: $('#mangasNameToAdd').val(),
 			date: new Date()
 		};
