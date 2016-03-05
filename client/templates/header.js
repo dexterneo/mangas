d1483a058ec37a3d4e3d1cc4119a467f6d70adc2
dexterneo <jeremyfourna@gmail.com>
2016-03-05 19:20:24 +0100

    Fix display on mobile screen

--- client/templates/header.js
+++ client/templates/header.js
@@ -28,3 +28,16 @@ Template.header.helpers({
 		}
 	}
 });
+
+Template.header.events({
+	'click .route': function() {
+		if ($('#navigation').hasClass('in')) {
+			$('#navigation').removeClass('in');
+		}
+	},
+	'click .navbar-brand': function() {
+		if ($('#navigation').hasClass('in')) {
+			$('#navigation').removeClass('in');
+		}
+	}
+});
