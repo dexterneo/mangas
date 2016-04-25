0afc2c2a2f05ca52398e9538d33d5ae064e1e5ab
dexterneo <jeremyfourna@gmail.com>
2016-04-25 23:15:21 +0200

    Add and update publications

--- server/publications/mangasDataPublications.js
+++ server/publications/mangasDataPublications.js
@@ -1,30 +1,38 @@
 // Send back all covers and french mangasName
-Meteor.publish('allMangasCover', function() {
+Meteor.publish('allMangasCover', () => {
 	return MangasData.find({}, {
 		fields: {
-			'cover': 1,
+			cover: 1,
 			'names.fr': 1
 		}
 	});
 });
 
-// Send back all mangasData
-Meteor.publish('allMangasData', function() {
-	return MangasData.find({});
-});
-
 // Send back one mangasData
-Meteor.publish('oneMangasData', function(id) {
-	return MangasData.find({
-		'_id': id
-	});
+Meteor.publish('oneMangasData', (id) => {
+	return MangasData.find({ _id: id });
 });
 
 // Send all Mangas for a given mangaka
-Meteor.publish('mangaka', function(author) {
+Meteor.publish('mangaka', (author) => {
 	var newAuthor = author.split(' ');
 	return MangasData.find({
 		'authors.firstName': newAuthor[0],
 		'authors.lastName': newAuthor[1]
+	}, {
+		fields: {
+			authors: 1,
+			tomes: 1
+		}
+	});
+});
+
+Meteor.publish('allMangasCoverForAdmin', () => {
+	return MangasData.find({}, {
+		fields: {
+			cover: 1,
+			'names.fr': 1,
+			tomes: 1
+		}
 	});
 });
