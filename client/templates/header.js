93c4f23b40feebd92498a309647ff08ad18ce8e9
dexterneo <jeremyfourna@gmail.com>
2016-04-25 18:02:29 +0200

    Update header

--- client/templates/header.js
+++ client/templates/header.js
@@ -1,26 +1,26 @@
 Template.header.helpers({
-	homeActive: function() {
+	homeActive() {
 		if (Router.current().route._path === '/ownedMangas') {
 			return 'active';
 		} else {
 			return false;
 		}
 	},
-	missingActive: function() {
+	missingActive() {
 		if (Router.current().route._path === '/missingMangas') {
 			return 'active';
 		} else {
 			return false;
 		}
 	},
-	adminActive: function() {
+	adminActive() {
 		if (Router.current().route._path.match('admin')) {
 			return 'active';
 		} else {
 			return false;
 		}
 	},
-	admin: function() {
+	admin() {
 		if (Meteor.user().profile.admin) {
 			return true;
 		} else {
