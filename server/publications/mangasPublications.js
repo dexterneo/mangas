4cce8de0d77cb856a2db4c6e72e7b7d75c473b78
dexterneo <jeremyfourna@gmail.com>
2016-04-25 18:04:26 +0200

    Update publication

--- server/publications/mangasPublications.js
+++ server/publications/mangasPublications.js
@@ -1,37 +1,57 @@
 // Send back all owned mangas
-Meteor.publish('allOwnedMangas', function(userId) {
+Meteor.publish('allOwnedMangas', (userId) => {
 	return Mangas.find({
-		"user": userId,
-		"owned": true
+		user: userId,
+		owned: true
+	}, {
+		fields: {
+			title: 1,
+			cover: 1,
+			name: 1,
+			number: 1,
+			owned: 1,
+			user: 1
+		}
 	});
 });
 
 // Send back all missing mangas
-Meteor.publish('allMissingMangas', function(userId) {
+Meteor.publish('allMissingMangas', (userId) => {
 	return Mangas.find({
-		"user": userId,
-		"owned": false
+		user: userId,
+		owned: false
+	}, {
+		fields: {
+			title: 1,
+			cover: 1,
+			name: 1,
+			number: 1,
+			user: 1,
+			owned: 1
+		}
 	});
 });
 
 // Send back the data about a tome
-Meteor.publish('tomeDetails', function(id) {
-	return Mangas.find({
-		"_id": id
-	});
+Meteor.publish('tomeDetails', (id) => {
+	return Mangas.find({ _id: id });
 });
 
 // Send back all tomes for a mangasName
-Meteor.publish('allTomes', function(userId, name) {
+Meteor.publish('allTomes', (userId, name) => {
 	return Mangas.find({
-		"user": userId,
-		"name": name
+		user: userId,
+		name
 	});
 });
 
 // Send back all tomes for a userId
-Meteor.publish('allTomesForUser', function(userId) {
-	return Mangas.find({
-		"user": userId
+Meteor.publish('allTomesForUser', (userId) => {
+	return Mangas.find({ user: userId }, {
+		fields: {
+			owned: 1,
+			number: 1,
+			user: 1
+		}
 	});
 });
