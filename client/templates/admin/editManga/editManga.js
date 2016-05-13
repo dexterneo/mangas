db636572903444845bb013debb84055739c7dd83
dexterneo <jeremyfourna@gmail.com>
2016-05-14 00:02:30 +0200

    WIP edition of a manga in the BO

--- client/templates/admin/editManga/editManga.js
+++ client/templates/admin/editManga/editManga.js
@@ -1,6 +1,34 @@
 Template.editManga.helpers({});
 
 Template.editManga.events({
+	'click #mangaTitlesSave' (event) {
+		event.preventDefault();
+		const data = {
+			mangaId: this._id,
+			fr: $('#namesFr').val(),
+			en: $('#namesEn').val(),
+			jp: $('#namesJp').val()
+		};
+		if (data.fr || data.en || data.jp) {
+			Meteor.call('updateMangaNames', data, (error, result) => {
+				if (error) {
+					return throwError(error.message);
+				}
+			});
+		}
+	},
+	'click #mangaAuthorsSave' (event) {
+		event.preventDefault();
+		console.log(this);
+	},
+	'click #mangaCoverSave' (event) {
+		event.preventDefault();
+		console.log(this);
+	},
+	'click #mangaGenreSave' (event) {
+		event.preventDefault();
+		console.log(this);
+	},
 	'click .tomeSave' (event) {
 		event.preventDefault();
 		console.log(this);
