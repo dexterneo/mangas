cda13ea3757aaebcdb71bfe90c64673452a2fae1
jeremyfourna <jeremyfourna@gmail.com>
2016-11-17 20:22:02 +0100

    Remove font

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,4 +1,3 @@
 import './dumbData.js';
-import './kadira.js';
 import './registerApi.js';
 import './newAccountConfig.js';
--- imports/startup/server/kadira.js
+++
@@ -1,3 +0,0 @@
-import { Kadira } from 'meteor/meteorhacks:kadira';
-
-Kadira.connect('qAGwrNFJ8eRNmwti5', '0fdccedb-503a-415b-bc8b-ee4865329bac');
